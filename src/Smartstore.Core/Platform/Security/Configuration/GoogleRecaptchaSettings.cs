feaf638f46090d792bf4ace20d8232a86d0ba7d4
darkluiggi <darkl@DARKLUIGGI_PC>
2025-10-29 00:54:06 -0500

    fix

---
+++ src/Smartstore.Core/Platform/Security/Configuration/GoogleRecaptchaSettings.cs
@@ -0,0 +1,32 @@
+﻿using Smartstore.Core.Configuration;
+
+namespace Smartstore.Core.Security
+{
+    public class GoogleRecaptchaSettings : ISettings
+    {
+        public const string DefaultWidgetUrl = "https://www.google.com/recaptcha/api.js";
+        public const string DefaultVerifyUrl = "https://www.google.com/recaptcha/api/siteverify";
+
+        public string WidgetUrl { get; set; }
+
+        public string VerifyUrl { get; set; }
+
+        public string SiteKey { get; set; }
+
+        public string SecretKey { get; set; }
+
+        /// <summary>
+        /// v2 | v3
+        /// </summary>
+        public string Version { get; set; } = "v2";
+
+        public bool UseDarkTheme { get; set; }
+
+        /// <summary>
+        /// normal | compact | invisible
+        /// </summary>
+        public string Size { get; set; } = "normal";
+
+        public float ScoreThreshold { get; set; } = 0.5f;
+    }
+}
